4d0158b721285bfb5ac94f6510c7872aed4f75ec
Alan Lau <alanlau97@gmail.com>
2018-12-28 00:32:11 -0500

    getting next page

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -4,7 +4,6 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
         'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',    
     ]
     
     def parse(self, response):
@@ -14,3 +13,16 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').extract_first(),
                 'tags': quote.css('div.tags a.tag::text').extract(),
             }
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            '''
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+            '''
+            # can be simplified as below
+            '''
+            yield response.follow(next_page, callback=self.parse)
+            '''
+            # can be simplified as below
+            for a in response.css('li.next a'):
+                yield response.follow(a, callback=self.parse)
